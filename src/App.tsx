0004a0377feb46d5b2a803b8da6dc1782155830c
manu <manuel@lonestone.io>
2025-03-11 14:20:01 +0100

    fix: router #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
 
+
 const App = () => {
-  const location = useLocation();
-  
   return (
-    <Router>
-      <Routes location={location} key={location.pathname}>
+    <Router basename="/ultimate-secure-chat">
+      <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="*" element={<Navigate to="/" replace />} />
