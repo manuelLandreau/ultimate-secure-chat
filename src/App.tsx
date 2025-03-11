836f68a866acdd86e50d7eaece3f8df56c68c809
manu <manuel@lonestone.io>
2025-03-11 14:15:32 +0100

    fix: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
 
 const App = () => {
+  const location = useLocation();
+  
   return (
     <Router>
-      <Routes>
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="*" element={<Navigate to="/" replace />} />
